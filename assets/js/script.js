7a3146724edc7e0f5b51475308385301a4d4c5af
Ben <ben.atkinson.official@gmail.com>
2021-09-18 13:13:22 +1000

    Display question logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,24 @@
 // scoped to the function expression wrapper, off the real global.
 (function () {
   // DOM elements
-  const startBtn = document.querySelector(".start-btn");
+  const el = {
+    start: {
+      container: document.querySelector(".start"),
+      button: document.querySelector(".start-btn"),
+    },
+    question: {
+      container: document.querySelector(".questions"),
+      header: document.querySelector(".questions h2"),
+      answerBtns: document.querySelectorAll(".answer-btn"),
+    },
+  };
 
   // Game variables
   let currentQuestion = 0;
   let score = 0;
 
   // Event listeners
-  startBtn.addEventListener("click", onStartClick);
+  el.start.button.addEventListener("click", onStartClick);
 
   function onStartClick() {
     console.log("start-btn clicked");
@@ -24,11 +34,23 @@
 
   function initQuiz() {
     console.log("initQuiz()");
+    // set score and question index back to initial values
     score = 0;
     currentQuestion = 0;
   }
 
-  function showQuestion(index) {
-    console.log("showQuestion() NYI");
+  function showQuestion(questionIndex) {
+    console.log("showQuestion()");
+    const currentQuestion = questions[questionIndex];
+    // first hide the start section
+    el.start.container.classList.add("hidden");
+    // unhide the questions sections
+    el.question.container.classList.remove("hidden");
+    // set question text into question header
+    el.question.header.textContent = currentQuestion.question;
+    // loop through the answer button elements and update text for each one
+    el.question.answerBtns.forEach((btn, index) => {
+      btn.textContent = currentQuestion.answers[index];
+    });
   }
 })(); // IIFE
