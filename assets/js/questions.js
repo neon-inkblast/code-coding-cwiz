7533315b270e2746b4ac8b235fb88778080f7756
Ben <ben.atkinson.official@gmail.com>
2021-09-18 12:51:27 +1000

    Add questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "The correct answer is A?",
+    answers: ["A", "B", "C", "D"],
+    correct: 0,
+  },
+  {
+    question: "The correct answer is B?",
+    answers: ["A", "B", "C", "D"],
+    correct: 1,
+  },
+  {
+    question: "The correct answer is D?",
+    answers: ["A", "B", "C", "D"],
+    correct: 3,
+  },
+];
