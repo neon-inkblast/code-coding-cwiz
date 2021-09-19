13cbddbae4d460502fb8ccf242dd3b611265f48b
Ben <ben.atkinson.official@gmail.com>
2021-09-19 14:28:01 +1000

    Add some JS questions to the question array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,37 @@
 const questions = [
   {
-    question: "The correct answer is A?",
-    answers: ["A", "B", "C", "D"],
-    correct: 0,
-  },
-  {
-    question: "The correct answer is B?",
-    answers: ["A", "B", "C", "D"],
+    question: "JavaScript was designed by",
+    answers: ["Martin Fowler", "Brendan Eich", "Bob Martin", "Douglas Crockford"],
     correct: 1,
   },
   {
-    question: "The correct answer is D?",
-    answers: ["A", "B", "C", "D"],
+    question: 'How would I run a function called "myFunction"?',
+    answers: ["myFunction", "run.myFunction", "myFunction()", "myfunction.run"],
+    correct: 2,
+  },
+  {
+    question: "Which of these functions will return the maximum of two values?",
+    answers: ["Math.ceil(x,y)", "Math.floor(x,y)", "Math.min(x,y)", "Math.max(x,y)"],
     correct: 3,
   },
   {
-    question: "The correct answer is B?",
-    answers: ["A", "B", "C", "D"],
-    correct: 1,
-  },
-  {
-    question: "The correct answer is A?",
-    answers: ["A", "B", "C", "D"],
+    question: "How can I get a random value in JavaScript?",
+    answers: ["Math.random()", "Math.rand()", "random()", "Math.getRandom()"],
     correct: 0,
   },
   {
-    question: "The correct answer is C?",
-    answers: ["A", "B", "C", "D"],
+    question: "Which of these operators compares for strict equality?",
+    answers: ["=", "==", "===", "equals()"],
     correct: 2,
+  },
+  {
+    question: "Which of these operations will increment the value of x by 1?",
+    answers: ["x++", "++x", "x += 1", "All of the above"],
+    correct: 3,
+  },
+  {
+    question: '"var x = true ? 3 : 4; " \nWhat is the value of x?',
+    answers: ["3", "4", "7", "true"],
+    correct: 0,
   },
 ];
