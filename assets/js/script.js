8134a6220e679f26ced0dcf2389be8c357c322e8
Ben <ben.atkinson.official@gmail.com>
2021-09-19 15:39:46 +1000

    Remove console.log, add comments to assist understanding code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 //Immediately invoked function expression (IIFE) pattern keeps our globals
 // scoped to the function expression wrapper, off the real global.
 (function () {
-  // DOM elements
+  // DOM elements, kept as an object for easy tracking in the IDE
   const el = {
+    // start screen
     start: {
       container: document.querySelector(".start"),
       button: document.querySelector(".start-btn"),
     },
+    // active quiz question screen
     question: {
       container: document.querySelector(".questions"),
       header: document.querySelector("#question-header"),
       answerBtns: document.querySelectorAll(".answer-btn"),
       feedback: document.querySelector(".feedback"),
     },
+    // end game screen
     results: {
       container: document.querySelector(".results"),
       input: document.querySelector("#initials-input"),
@@ -20,14 +23,19 @@
       submitButton: document.querySelector("#submit"),
       tryAgainButton: document.querySelector("#try-again"),
     },
+    // game stats
     timer: document.getElementById("timer-span"),
     score: document.getElementById("score-span"),
   };
 
   // Game variables constants
+  // how much to add to score on correct
   const scoreIncrement = 10;
+  // how much to substract from timer on incorrect
   const timerIncrement = 10;
+  // how many seconds to start the quiz with
   const timerInitialValue = 180;
+  // keep the storage key as a variable to reduce typos and make easier to update
   const storageKey = "highscores";
   // Game variables
   let currentQuestion = 0;
@@ -42,39 +50,104 @@
   el.question.answerBtns.forEach((btn, index) => {
     btn.addEventListener("click", checkAnswerFn(index));
   });
-  // high score button
+  // end game high score submit button
   el.results.submitButton.addEventListener("click", onScoreSubmit);
+  // end game try again button - reuse start game click handler
   el.results.tryAgainButton.addEventListener("click", onStartClick);
+
+  //--------------------------Click handlers-------------------------//
 
   function onStartClick(event) {
     event.preventDefault();
-    console.log("start-btn clicked");
     startQuiz();
   }
 
+  function checkAnswerFn(index) {
+    // create and return a function to be run by the click handler on an element
+    // this way we can generate a new function for each handler based on the index passed into this function
+    return function () {
+      // create a new element for answer feedback "correct" or "incorrect"
+      // create a new element for answer feedback "correct" or "incorrect"
+      const feedbackEl = document.createElement("div");
+      // if answer correct, do correct answer logic
+      if (questions[currentQuestion].correct === index) {
+        correctAnswer(feedbackEl);
+      } else {
+        // if answer not correct, do incorrect answer logic
+        incorrectAnswer(feedbackEl);
+      }
+      // append the feedback text into the feedback container element
+      el.question.feedback.appendChild(feedbackEl);
+      // set up a timer to remove the newly created feedback text after a short delay
+      delayedRemoveFeedback(feedbackEl);
+      // move on to the next question
+      nextQuestion();
+    };
+  }
+
+  function onScoreSubmit(event) {
+    event.preventDefault();
+    // load high scores from storage, or set to an empty array if null
+    const highScoreList = loadScores() || [];
+    // get value from input form
+    const initials = el.results.input.value;
+    // if the value is falsy (null, "", etc), display validation message
+    if (!initials) {
+      el.results.validation.textContent = "You must enter a value";
+      // otherwise
+    } else {
+      // clear validation message
+      el.results.validation.textContent = "";
+      // disable submit button so that it can't be entered multiple times
+      el.results.submitButton.disabled = true;
+      // create a new highscore object
+      const highScore = {
+        initials: initials.toUpperCase(),
+        score,
+      };
+      // add it to the existing list
+      highScoreList.push(highScore);
+      // sort the list using Array.sort()
+      highScoreList.sort((a, b) => {
+        // by score first
+        const result = b.score - a.score;
+        // then if score is the same
+        if (result === 0) {
+          // sort by initials
+          return a.initials.localeCompare(b.initials);
+        }
+        return result;
+      });
+      // save score list into local storage
+      saveScores(highScoreList);
+      // update the score list view on the results screen
+      updateHighScores(highScoreList);
+      // navigate to the seperate high scores page after 2 seconds
+      setTimeout(navigateToHighScores, 2000);
+    }
+  }
+
+  //--------------------------Game logic-------------------------//
   function startQuiz() {
-    console.log("startQuiz()");
     initQuiz();
     startTimer();
     showQuestion();
   }
 
   function initQuiz() {
-    console.log("initQuiz()");
-    // set score and question index back to initial values
+    // set score, timer and question index back to initial values
     score = 0;
     currentQuestion = 0;
     timer = timerInitialValue;
-
+    // re-enable the submit button and clear previous entry
     el.results.submitButton.disabled = false;
     el.results.input.value = "";
-
+    // update the view
     updateScoreDisplay();
     updateTimerDisplay();
   }
 
   function showQuestion() {
-    console.log("showQuestion()");
     const question = questions[currentQuestion];
     // first hide the start section
     el.start.container.classList.add("hidden");
@@ -89,67 +162,14 @@
     });
   }
 
-  function checkAnswerFn(index) {
-    return function () {
-      const feedbackEl = document.createElement("div");
-      if (questions[currentQuestion].correct === index) {
-        correctAnswer(feedbackEl);
-      } else {
-        incorrectAnswer(feedbackEl);
-      }
-      el.question.feedback.appendChild(feedbackEl);
-      delayedRemoveFeedback(feedbackEl);
-      nextQuestion();
-    };
-  }
-
-  function correctAnswer(element) {
-    score += scoreIncrement;
-    updateScoreDisplay();
-    element.classList.add("correct");
-    element.textContent = "Correct!";
-  }
-
-  function incorrectAnswer(element) {
-    timer -= timerIncrement;
-    updateTimerDisplay();
-    element.classList.add("incorrect");
-    element.textContent = "Wrong!";
-  }
-
-  function delayedRemoveFeedback(element) {
-    setTimeout(function () {
-      element.remove();
-    }, 2000);
-  }
-
-  function nextQuestion() {
-    currentQuestion++;
-    if (currentQuestion >= questions.length) {
-      endQuiz();
-    } else {
-      showQuestion();
-    }
-  }
-
-  function updateTimerDisplay() {
-    const min = Math.floor(timer / 60);
-    const sec = timer % 60;
-    el.timer.textContent = `${min}m ${sec}s`;
-  }
-
-  function updateScoreDisplay() {
-    el.score.textContent = `${score} points`;
-  }
-
   function startTimer() {
-    console.log("startTimer()");
+    // set the interval handle into holding variable
     timerHandle = setInterval(onTimerTick, 1000);
 
     function onTimerTick() {
-      console.log("onTimer()");
+      // update timer view
       updateTimerDisplay();
-
+      // if timer has run out, do end quiz
       if (timer <= 0) {
         endQuiz();
       } else {
@@ -159,15 +179,72 @@
   }
 
   function endQuiz() {
-    console.log("endQuiz()");
+    // clear out the timer
     clearInterval(timerHandle);
+    // hide questions
     el.question.container.classList.add("hidden");
+    // show score entry form
     el.results.container.classList.remove("hidden");
+  }
+
+  function correctAnswer(element) {
+    // add to score
+    score += scoreIncrement;
+    // update score view
+    updateScoreDisplay();
+    // fill in details of feedback element
+    element.classList.add("correct");
+    element.textContent = "Correct!";
+  }
+
+  function incorrectAnswer(element) {
+    // decrease time remaining
+    timer -= timerIncrement;
+    // update time view
+    updateTimerDisplay();
+    // fill in details of feedback element
+    element.classList.add("incorrect");
+    element.textContent = "Wrong!";
+  }
+
+  function delayedRemoveFeedback(element) {
+    // after 2 seconds, remove feedback displayed for previous answer
+    setTimeout(function () {
+      element.remove();
+    }, 2000);
+  }
+
+  function nextQuestion() {
+    // increase question index counter by 1
+    currentQuestion++;
+    // check if index is more than number of questions
+    if (currentQuestion >= questions.length) {
+      // if so, go to end quiz
+      endQuiz();
+    } else {
+      // if not, show the next question
+      showQuestion();
+    }
+  }
+
+  //--------------------------View update logic-------------------------//
+  function updateTimerDisplay() {
+    // update the time remaining displayed on screen
+    const min = Math.floor(timer / 60);
+    const sec = timer % 60;
+    el.timer.textContent = `${min}m ${sec}s`;
+  }
+
+  function updateScoreDisplay() {
+    // update the score displayed on screen
+    el.score.textContent = `${score} points`;
   }
 
   function updateHighScores(scores) {
     const scoreList = document.getElementById("scores-list");
+    // empty the existing list element of all content
     scoreList.textContent = "";
+    // generate a list item for each score and append it to the score list
     scores.forEach((score) => {
       const newItem = document.createElement("li");
       newItem.textContent = `${score.initials} - ${score.score}`;
@@ -175,42 +252,18 @@
     });
   }
 
-  function onScoreSubmit(event) {
-    event.preventDefault();
-    console.log("onScoreSubmit()");
-    const highScoreList = loadScores() || [];
-    const initials = el.results.input.value;
-    if (!initials) {
-      el.results.validation.textContent = "You must enter a value";
-    } else {
-      el.results.validation.textContent = "";
-      el.results.submitButton.disabled = true;
-      const highScore = {
-        initials: initials.toUpperCase(),
-        score,
-      };
-      highScoreList.push(highScore);
-      highScoreList.sort((a, b) => {
-        const result = b.score - a.score;
-        if (result === 0) {
-          return a.initials.localeCompare(b.initials);
-        }
-        return result;
-      });
-      saveScores(highScoreList);
-      updateHighScores(highScoreList);
-      setTimeout(navigateToHighScores, 2500);
-    }
-  }
-
+  //--------------------------LocalStorage abstraction functions-------------------------//
   function loadScores() {
+    // use JSON parse to convert localstorage value from string to array
     return JSON.parse(localStorage.getItem(storageKey));
   }
 
   function saveScores(scores) {
+    // use JSON stringify to convert score array to a string for storing in localstorage
     localStorage.setItem(storageKey, JSON.stringify(scores));
   }
 
+  //--------------------------Navigation functions-------------------------//
   function navigateToHighScores() {
     window.location.href = "./highscores.html";
   }
